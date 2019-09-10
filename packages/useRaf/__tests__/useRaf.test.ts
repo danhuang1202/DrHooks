db28a5284d726aa4c89ee7d54b8ea8a5c45f9093
Dan Huang <danhuang@verizonmedia.com>
2019-09-11 00:28:45 +0800

    feat: add test case

--- packages/useRaf/__tests__/useRaf.test.ts
+++ packages/useRaf/__tests__/useRaf.test.ts
@@ -2,12 +2,4 @@ describe('useRaf', () => {
   it('test1', () => {
     expect(true).toEqual(true)
   })
-
-  it('test2', () => {
-    expect(true).toEqual(true)
-  })
-
-  it('test3', () => {
-    expect(true).toEqual(true)
-  })
 })
