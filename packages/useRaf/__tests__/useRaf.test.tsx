8b6d6f7da95ab334187105a2151a78524d00fed6
Dan Huang <danhuang@verizonmedia.com>
2019-09-11 14:23:47 +0800

    test: add test case

--- packages/useRaf/__tests__/useRaf.test.tsx
+++ packages/useRaf/__tests__/useRaf.test.tsx
@@ -1,19 +1,68 @@
 import React from 'react'
 import useRaf from '@src/index.ts'
-import { render } from '@testing-library/react'
+import { render, fireEvent, cleanup } from '@testing-library/react'
+
+const TEST_ID_STATUS = 'status'
+const TEST_ID_START = 'start'
+const TEST_ID_STOP = 'stop'
 
 const callback = jest.fn()
-const Test = ({}) => {
-  const {isActive} = useRaf({disable: true, callback})
+const Test = ({disable = true, duration}) => {
+  const {isActive, start, stop} = useRaf({disable, duration, callback})
 
   return (
-    <div data-testid="test">{`${isActive}`}</div>
+    <div>
+      <span  data-testid={TEST_ID_STATUS}>{`${isActive}`}</span>
+      <span  data-testid={TEST_ID_START} onClick={start}/>
+      <span  data-testid={TEST_ID_STOP} onClick={stop}/>
+    </div> 
   )
 }
 
 describe('useRaf', () => {
+  afterEach(() => {
+    cleanup()
+  })
+
   it('isActive should update after call start() and stop()', () => {
     const { getByTestId } = render(<Test />)
-    expect(getByTestId('test').textContent).toBe('false')
+    expect(getByTestId(TEST_ID_STATUS).textContent).toBe('false')
+
+    fireEvent.click(getByTestId(TEST_ID_START))
+    expect(getByTestId(TEST_ID_STATUS).textContent).toBe('true')
+
+    fireEvent.click(getByTestId(TEST_ID_STOP))
+    expect(getByTestId(TEST_ID_STATUS).textContent).toBe('false')
+  })
+
+  it('should run requestAnimationFrame at the first place when disable is false', () => {
+    const { getByTestId } = render(<Test disable={false}/>)
+    expect(getByTestId(TEST_ID_STATUS).textContent).toBe('true')
+  })
+
+  it.only('should cancelAnimationFrame when time reach duration', () => {
+    const duration = 1000
+    let time = Date.now()
+    jest.spyOn(window, 'requestAnimationFrame').mockImplementation(callback => {
+      time += 500
+      setTimeout(() => {
+        callback(time)
+      }, 0);
+      
+      return time
+    })
+
+    jest.useFakeTimers()
+
+    const { getByTestId } = render(<Test duration={duration}/>)
+    expect(getByTestId(TEST_ID_STATUS).textContent).toBe('false')
+
+    fireEvent.click(getByTestId(TEST_ID_START))
+    expect(getByTestId(TEST_ID_STATUS).textContent).toBe('true')
+
+    jest.advanceTimersByTime(1000)
+    expect(getByTestId(TEST_ID_STATUS).textContent).toBe('false')
+
+    jest.useRealTimers()
   })
 })
